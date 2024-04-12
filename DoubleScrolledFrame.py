e6029db2a9842015a2cbee597b95f7a08be4369d
Alejandro de la Vega <aleph4@gmail.com>
2024-04-12 14:20:54 -0700

    Run black

--- DoubleScrolledFrame.py
+++ DoubleScrolledFrame.py
@@ -1,10 +1,10 @@
-
 try:
     import tkinter as tk
     from tkinter import ttk
 except ImportError:
     import Tkinter as tk
     import ttk
+
 
 class DoubleScrolledFrame:
     """
@@ -17,32 +17,35 @@ class DoubleScrolledFrame:
     if you subclass this there is no built in way for the children to access it.
     You need to provide the controller separately.
     """
+
     def __init__(self, master, **kwargs):
-        width = kwargs.pop('width', None)
-        height = kwargs.pop('height', None)
+        width = kwargs.pop("width", None)
+        height = kwargs.pop("height", None)
         self.outer = tk.Frame(master, **kwargs)
 
         self.vsb = ttk.Scrollbar(self.outer, orient=tk.VERTICAL)
-        self.vsb.grid(row=0, column=1, sticky='ns')
+        self.vsb.grid(row=0, column=1, sticky="ns")
         self.hsb = ttk.Scrollbar(self.outer, orient=tk.HORIZONTAL)
-        self.hsb.grid(row=1, column=0, sticky='ew')
-        self.canvas = tk.Canvas(self.outer, highlightthickness=0, width=width, height=height)
-        self.canvas.grid(row=0, column=0, sticky='nsew')
+        self.hsb.grid(row=1, column=0, sticky="ew")
+        self.canvas = tk.Canvas(
+            self.outer, highlightthickness=0, width=width, height=height
+        )
+        self.canvas.grid(row=0, column=0, sticky="nsew")
         self.outer.rowconfigure(0, weight=1)
         self.outer.columnconfigure(0, weight=1)
-        self.canvas['yscrollcommand'] = self.vsb.set
-        self.canvas['xscrollcommand'] = self.hsb.set
+        self.canvas["yscrollcommand"] = self.vsb.set
+        self.canvas["xscrollcommand"] = self.hsb.set
         # mouse scroll does not seem to work with just "bind"; You have
         # to use "bind_all". Therefore to use multiple windows you have
         # to bind_all in the current widget
         self.canvas.bind("<Enter>", self._bind_mouse)
         self.canvas.bind("<Leave>", self._unbind_mouse)
-        self.vsb['command'] = self.canvas.yview
-        self.hsb['command'] = self.canvas.xview
+        self.vsb["command"] = self.canvas.yview
+        self.hsb["command"] = self.canvas.xview
 
         self.inner = tk.Frame(self.canvas)
         # pack the inner Frame into the Canvas with the topleft corner 4 pixels offset
-        self.canvas.create_window(4, 4, window=self.inner, anchor='nw')
+        self.canvas.create_window(4, 4, window=self.inner, anchor="nw")
         self.inner.bind("<Configure>", self._on_frame_configure)
 
         self.outer_attr = set(dir(tk.Widget))
@@ -59,7 +62,7 @@ class DoubleScrolledFrame:
         x1, y1, x2, y2 = self.canvas.bbox("all")
         height = self.canvas.winfo_height()
         width = self.canvas.winfo_width()
-        self.canvas.config(scrollregion = (0,0, max(x2, width), max(y2, height)))
+        self.canvas.config(scrollregion=(0, 0, max(x2, width), max(y2, height)))
 
     def _bind_mouse(self, event=None):
         self.canvas.bind_all("<4>", self._on_mousewheel)
@@ -70,35 +73,40 @@ class DoubleScrolledFrame:
         self.canvas.unbind_all("<4>")
         self.canvas.unbind_all("<5>")
         self.canvas.unbind_all("<MouseWheel>")
-        
+
     def _on_mousewheel(self, event):
         """Linux uses event.num; Windows / Mac uses event.delta"""
-        func = self.canvas.xview_scroll if event.state & 1 else self.canvas.yview_scroll 
+        func = self.canvas.xview_scroll if event.state & 1 else self.canvas.yview_scroll
         if event.num == 4 or event.delta > 0:
-            func(-1, "units" )
+            func(-1, "units")
         elif event.num == 5 or event.delta < 0:
-            func(1, "units" )
-    
+            func(1, "units")
+
     def __str__(self):
         return str(self.outer)
+
 
 #  **** SCROLL BAR TEST *****
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Scrollbar Test")
-    root.geometry('400x500')
-    lbl = tk.Label(root, text="Hold shift while using the scroll wheel to scroll horizontally")
+    root.geometry("400x500")
+    lbl = tk.Label(
+        root, text="Hold shift while using the scroll wheel to scroll horizontally"
+    )
     lbl.pack()
-    
+
     # use the Scrolled Frame just like any other Frame
-    frame = DoubleScrolledFrame(root, width=300, borderwidth=2, relief=tk.SUNKEN, background="light gray")
-    #frame.grid(column=0, row=0, sticky='nsew') # fixed size
-    frame.pack(fill=tk.BOTH, expand=True) # fill window
+    frame = DoubleScrolledFrame(
+        root, width=300, borderwidth=2, relief=tk.SUNKEN, background="light gray"
+    )
+    # frame.grid(column=0, row=0, sticky='nsew') # fixed size
+    frame.pack(fill=tk.BOTH, expand=True)  # fill window
 
     for i in range(30):
         for j in range(20):
-            label = tk.Label(frame, text="{}{}".format(alphabet[j], i), relief='ridge')
-            label.grid(column=j, row=i, sticky='ew', padx=2, pady=2)
+            label = tk.Label(frame, text="{}{}".format(alphabet[j], i), relief="ridge")
+            label.grid(column=j, row=i, sticky="ew", padx=2, pady=2)
 
     root.mainloop()
--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import ttkbootstrap as tb
-from ttkbootstrap.constants import *
+from ttkbootstrap.constants import BOTH, YES
 from ttkbootstrap.validation import add_regex_validation
 from ttkbootstrap.style import Bootstyle
 from ttkbootstrap.scrolled import ScrolledFrame
@@ -9,16 +9,16 @@ from functools import partial
 
 
 from DoubleScrolledFrame import DoubleScrolledFrame
-from input_setup import(CreateInputWidgets)
-from nodes_transformations_setup_try_tabs import AddNode
+from input_setup import CreateInputWidgets
+from nodes_transformations_setup import AddNode
 from utils import CollapsingFrame, make_button
 from make_json import make_json
 
 from edges_setup import AddEdgeWidgets
 
 # GUI window
-main_window = tb.Window(themename = 'superhero', minsize=(300, 1))
-main_window.title('BidsSM maker')
+main_window = tb.Window(themename="superhero", minsize=(300, 1))
+main_window.title("BidsSM maker")
 main_window.geometry("1400x950")
 
 # Turn it into a scrolled frame
@@ -30,27 +30,28 @@ cf = CollapsingFrame(scrolling_window)
 cf.pack(fill=BOTH)
 label_width = 30
 s = tb.Style()
-s.configure('.', font=('Helvetica', 18))
+s.configure(".", font=("Helvetica", 18))
 
 # Inputs Section
 top_level_frame = tb.Frame(cf, padding=10)
 top_level_data = CreateInputWidgets(top_level_frame)
-cf.add(child=top_level_frame, title='Top level data')
+cf.add(child=top_level_frame, title="Top level data")
 
 # Nodes
 nodes_frame = tb.Frame(cf, padding=10)
 node_widgets = AddNode(nodes_frame)
-cf.add(child=nodes_frame, title='Nodes')
+cf.add(child=nodes_frame, title="Nodes")
 
 # Edges
 edges_frame = tb.Frame(cf, padding=10)
 edge_widgets = AddEdgeWidgets(edges_frame)
-cf.add(child=edges_frame, title='Edges')
+cf.add(child=edges_frame, title="Edges")
 
 # Button to display json
-make_button(main_window, 'Show Model Spec!', partial(make_json,
-                                                      top_level_data,
-                                                      node_widgets,
-                                                      edge_widgets))
+make_button(
+    main_window,
+    "Show Model Spec!",
+    partial(make_json, top_level_data, node_widgets, edge_widgets),
+)
 
-main_window.mainloop() 
+main_window.mainloop()
